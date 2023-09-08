430c98b9f78c81284999b03ecc9b8b3145f4f19c
kalappagari-bharath <119477208+kalappagari-bharath@users.noreply.github.com>
2023-09-08 10:23:36 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(80, '0.0.0.0', () => console.log('Example app listening on port 3000!'))
